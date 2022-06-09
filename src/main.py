7c1607ee3caf3ae892e6a7a3e020a1f3deebf9cf
mateo <mdujic212@gmail.com>
2022-06-09 17:11:52 +0200

    napisan lekser(bez jednolinijskih komentara)

--- src/main.py
+++ src/main.py
@@ -8,10 +8,33 @@ class T(TipoviTokena):
     IF, THEN, ELSE, ENDIF, OOTV, OZATV = 'if', 'then', 'else', 'endif', '(', ')'
     FUN, ENDFUN, RETURN = 'fun', 'endfun', 'return'
     INPUT = 'input'
-    OKOMENT, ZKOMENT = '/*', '*/'
 
     
     class BROJ(Token): pass
     class IME(Token):pass
     class STRING(Token): pass
-    
+
+@lexer
+def snail(lex):
+    for znak in lex:
+        if znak.isspace():
+            lex.zanemari()
+        elif znak.isalpha():
+            lex * str.isalnum
+            yield lex.literal_ili(T.IME)
+        elif znak.isdecimal():
+            lex.prirodni_broj(znak)
+            yield lex.token(T.BROJ)
+        elif znak == '"':
+            lex - '"'
+            yield lex.token(T.STRING)
+        elif znak == '~':
+            lex.pročitaj_do('~', više_redova = True)
+            lex.zanemari()
+        else:
+            lex.literal(T)
+
+
+snail('''
+print newline;
+''')
