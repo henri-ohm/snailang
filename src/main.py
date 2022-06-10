24c797c9ba4c4bc8aebc7bdbcba54157db89cf0c
henri-ohm <mh51344@fer.hr>
2022-06-10 10:00:05 +0200

    sanirana klasa za tokene, sanirani elifovi u lexeru, zamijenjen viselinijski komentar s ~

--- src/main.py
+++ src/main.py
@@ -1,24 +1,35 @@
 from vepar import *
 
-class T(TipoviTokena):
-    PLUS, MINUS, PUTA, PODIJELJENO = '+-*/'
-    MANJE, VISE, MANJEJ, VISEJ = '<', '>', '<=', '>='
-    JJEDNAKO, RAZLICITO = '==', '!='
-    PRIDRUZI, PRINT, NEWLINE, TOCKAZAREZ = '=', 'print', 'newline', ';'    
-    IF, THEN, ELSE, ENDIF, OOTV, OZATV = 'if', 'then', 'else', 'endif', '(', ')'
-    FUN, ENDFUN, RETURN = 'fun', 'endfun', 'return'
-    INPUT = 'input'
 
-    
-    class BROJ(Token): pass
-    class IME(Token):pass
-    class STRING(Token): pass
+class T(TipoviTokena):
+    PLUS, MINUS, PUTA, PODIJELJENO, MANJE, VISE, PRIDRUZI, ZAREZ, TOCKAZAREZ, DVOTOCKA, OOTV, OZATV = '+-*/<>=,;:()'
+    MANJEJ, VISEJ, JJEDNAKO, RAZLICITO = '<=', '>=', '==', '!='
+    PRINT, NEWLINE, INPUT = 'print', 'newline', 'input'
+    IF, THEN, ELSE, ENDIF = 'if', 'then', 'else', 'endif'
+    FUN, ENDFUN, RETURN = 'fun', 'endfun', 'return'
+
+    class BROJ(Token):
+        pass
+    class IME(Token):
+        pass
+    class STRING(Token):
+        pass
+
 
 @lexer
 def snail(lex):
     for znak in lex:
         if znak.isspace():
             lex.zanemari()
+        elif znak == '<':
+            yield lex.token(T.MANJEJ if lex >= '=' else T.MANJE)
+        elif znak == '>':
+            yield lex.token(T.VISEJ if lex >= '=' else T.VISE)
+        elif znak == '=':
+            yield lex.token(T.JJEDNAKO if lex >= '=' else T.PRIDRUZI)
+        elif znak == '!':
+            lex >> '='
+            lex.token(T.RAZLICITO)
         elif znak.isalpha():
             lex * str.isalnum
             yield lex.literal_ili(T.IME)
@@ -28,41 +39,34 @@ def snail(lex):
         elif znak == '"':
             lex - '"'
             yield lex.token(T.STRING)
-        elif znak == '<':
-            if lex >= '=': yield lex.token(T.MANJEJ)
-            else: yield lex.token(T.MANJE)
-        elif znak == '>':
-            if lex >= '=': yield lex.token(T.VECEJ)
-            else: yield lex.token(T.VECE)
-        elif znak == '=':
-            if lex >= '=': yield lex.token(T.JJEDNAKO)
-            else: yield lex.token(T.PRIDRUZI)
-        elif znak == '!':
-            lex >> '='
-            yield lex.token(T.RAZLICITO)
-        elif znak == '/':
-            if lex >= '*':
-                while True:
-                    lex.pročitaj_do('*', više_redova = True)
-                    if(lex >= '/'): break
-            elif lex >= '/':
-                lex - '\n'
-            else:
-                yield lex.token(T.PODIJELJENO)                
+        elif znak == "~":
+            lex.pročitaj_do("~", više_redova=True)
             lex.zanemari()
+        elif znak == '/':
+            if lex >= '/':
+                lex - '\n'
+                lex.zanemari()
+            else:
+                yield lex.token(T.PODIJELJENO)
         else:
             yield lex.literal(T)
 
 
 snail('''
-print newline;
-''')
-snail('''
-print i*i; // print the square of i
-if i != v/2 then /* is i 
-the half of v?*/
-print newline; //yes
+fun fib(x, y):
+    ~ ova funkcija racuna fibonacijeve brojeve
+    i pise ih u beskonacnost ~
+    print x;
+    print newline;
+    fib(y, x+y);
+endfun
+
+x = 1/1 // postavi x na nula
+fib(x,x)
+
+if x >= 0 then
+    x = 1
 else
-print "--"; //no
-endif
+    x = 2
+
 ''')
