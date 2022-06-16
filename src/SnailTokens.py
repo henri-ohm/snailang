3fdbdd2e2095250b136b4f389eeab551452b77ff
henri-ohm <mh51344@fer.hr>
2022-06-16 16:17:38 +0200

    Tokeni premjesteni u drugi file

---
+++ src/SnailTokens.py
@@ -0,0 +1,21 @@
+from vepar import TipoviTokena, Token
+
+
+class T(TipoviTokena):
+    PLUS, MINUS, PUTA, PODIJELJENO, MANJE, VISE, PRIDRUZI, ZAREZ, TOCKAZAREZ, DVOTOCKA, OOTV, OZATV = '+-*/<>=,;:()'
+    MANJEJ, VISEJ, JJEDNAKO, RAZLICITO = '<=', '>=', '==', '!='
+    PRINT, NEWLINE, INPUT = 'print', 'newline', 'input'
+    IF, THEN, ELSE, ENDIF = 'if', 'then', 'else', 'endif'
+    FUN, ENDFUN, RETURN = 'fun', 'endfun', 'return'
+
+    class BROJ(Token):
+        def vrijednost(self, mem):
+            return int(self.sadržaj)
+
+    class IME(Token):
+        def vrijednost(self, mem):
+            return mem[self]
+
+    class STRING(Token):
+        def vrijednost(self, mem):
+            return self.sadržaj[1:-1]
