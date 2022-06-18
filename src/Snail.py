0a7270ec961f0d60c2f377315f168d44f42118b6
henri-ohm <mh51344@fer.hr>
2022-06-18 15:55:05 +0200

    Maknuti testovi. Testirati možemo pokretanjem Snail.py sa zadnjim argumentom "-v".

--- src/Snail.py
+++ src/Snail.py
@@ -1,7 +1,8 @@
 import sys
 
-from vepar import Memorija
+from vepar import Memorija, prikaz
 from SnailParser import P
+from SnailLexer import snail
 
 
 def izvrsi(src: str):
@@ -12,8 +13,18 @@ def izvrsi(src: str):
 """
 Pozicionirati se u direktorij 'snailang'.
 Primjer pokretanja:
-$ python3 src/Snail.py examples/faktorijel.snail
+$ python3 src/Snail.py examples/07-faktorijel.snail
 """
 if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("usage: python3 src/Snail.py SRC [-v] ")
     with open(sys.argv[1]) as src_file:
-        izvrsi(src_file.read())
+        src = src_file.read()
+
+        if len(sys.argv) == 3 and sys.argv[2] == "-v":
+            print("-----------------LEX---------------")
+            print(snail(src))
+            print("----------------PARSE--------------")
+            print(prikaz(P(src)))
+            print("----------------EXEC---------------")
+        izvrsi(src)
